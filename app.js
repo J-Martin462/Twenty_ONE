050765efe339e770f1ec383c06d50f4a283add74
J Martin <jesmartindelcampo@leisdstudent.ws>
2022-04-20 18:37:43 -0700

    Getting the cards on screen

--- app.js
+++ app.js
@@ -12,3 +12,10 @@ function fold (params) {
 }
 
 
+
+function hit(){
+    var r = Math.floor(Math.random() * 10) + 1
+    var suits = [ "C", "D", "H", "S"]
+    var ransuit = Math.floor(Math.random() * 4)
+    document.getElementById("carddeck").innerHTML = ("<div class=card> <img src=images/"+ r + suits[ransuit] +".jpg class=card-img-top alt=..> </div>")
+}
