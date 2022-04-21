8a57b0ba918a7dc745bf2dc6b56ccb7f6366b9c3
J Martin <jesmartindelcampo@leisdstudent.ws>
2022-04-21 13:24:30 -0500

    First attempt at making a hand

--- app.js
+++ app.js
@@ -14,8 +14,10 @@ function fold (params) {
 
 
 function hit(){
+    var deckArray = []
     var r = Math.floor(Math.random() * 10) + 1
     var suits = [ "C", "D", "H", "S"]
     var ransuit = Math.floor(Math.random() * 4)
-    document.getElementById("carddeck").innerHTML = ("<div class=card> <img src=images/"+ r + suits[ransuit] +".jpg class=card-img-top alt=..> </div>")
+    deckArray.push("<div class=card> <img src=images/"+ r + suits[ransuit] +".jpg class=card-img-top alt=..> </div>")
+    document.getElementById("carddeck").innerHTML = (deckArray[0,1,2,3,4,5,6,7,8,9,10])
 }
