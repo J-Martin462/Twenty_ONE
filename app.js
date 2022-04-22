7c792ff05aba418067e04f2df3cc84af7599a313
J Martin <jesmartindelcampo@leisdstudent.ws>
2022-04-22 13:51:08 -0500

    First game atempt

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var q = 0
 function hit() {
     let ran = Math.floor(Math.random() * 21);
 }
@@ -7,17 +8,28 @@ function reset(params) {
     
 }
 
-function fold (params) {
+function fold(params) {
     
 }
 
-function hit(){
+function usrgm(){
     var r = Math.floor(Math.random() * 10) + 1
     var suits = [ "C", "D", "H", "S"]
     var ransuit = Math.floor(Math.random() * 4)
     document.getElementById("carddeck").insertAdjacentHTML ("beforeend", "<img src=Cards/"+ r + suits[ransuit] +".jpg class=card-img-top alt=..>")
     return r
 }
+
+function hit(){
+    var q = 0
+    q = usrgm() + q
+    if (q > 20) {
+        document.getElementById("jumbo").innerText = ("You BUSTED. Reset.")
+        
+    }
+    return q
+}
+
 
 function compGam() {
     var r = Math.floor(Math.random() * 10) + 1
@@ -39,13 +51,16 @@ function compLogic() {
     return cpuscr
 }
 
-function fullGame(a) {
+function fullGame(a, y) {
+
+
+    var skip = "e"
         if(a < 20){     /* change score limit here */
             document.getElementById("jumbo").innerText = ("The computer has chose to stand")
 
-        }else if(x == 20){  /* change score limit here */
+        }else if(a == 20){  /* change score limit here */
             document.getElementById("jumbo").innerText = ("The computer has 20 points and can win unless you have 20 points the computer wins ")
-        }else if(x > 20){   /* change score limit here */
+        }else if(a > 20){   /* change score limit here */
             document.getElementById("jumbo").innerText = ("The computer busted. You win. :{ ")
             skip = "true"
             usrScore.push(1)
@@ -55,18 +70,17 @@ function fullGame(a) {
 
             var y = usrGame()
             if(x > y){
-                alert(`Congradulations! You lost. ${x} to ${y}`)
+                alert(`Congradulations! You lost. ${a} to ${y}`)
                 compScore.push(1)
             }else if (y > 20){/* change score limit here*/
                 alert("oof, you busted. The computer WINS!")
                 compScore.push(1)
             }else if(x < y){
-                alert(`Yeah, Yeah, Yeah, you won. ${y} to ${x}`)
+                alert(`Yeah, Yeah, Yeah, you won. ${y} to ${a}`)
                 usrScore.push(1)
             }else{
-                alert( `We tied. No points. ${x} to ${y}`)
+                alert( `We tied. No points. ${a} to ${y}`)
             }
         }
 }
-
-compLogic()
+fullGame(compLogic(), hit())
